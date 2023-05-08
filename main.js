29e1daecc1c1d40c848870ba3e6aaff7a905ec98
lucas alves <114693104+lucaslanpdsa@users.noreply.github.com>
2023-05-08 15:40:10 -0300

    retirando a marcara do campo e-mail

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 $(document).ready(function () {
-  $("#e-mail").mask("00000-000", {placeholder : "00000-000"})
   $("#telefone").mask("(00) 0 0000-0000", {placeholder : "(00) 0 0000-0000"})
   $("#cpf").mask('000.000.000-00', {reverse: true})
   $("#cep").mask("00000-000", {placeholder : "00000-000"})
