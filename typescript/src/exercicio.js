851443916f672120bc2c624999583aa2a3e22e16
lucas alves <114693104+lucaslanpdsa@users.noreply.github.com>
2023-09-12 12:38:29 -0300

    exercício_ts

---
+++ typescript/src/exercicio.js
@@ -0,0 +1,11 @@
+"use strict";
+function multiplicacao(fator1, fator2) {
+    let resultado = fator1 * fator2;
+    return resultado;
+}
+function saudacao(nome) {
+    let retorno = `Olá ${nome}`;
+    return retorno;
+}
+console.log(multiplicacao(25, 68));
+console.log(saudacao("gean"));
---
+++ typescript/src/exercicio.ts
@@ -0,0 +1,12 @@
+function multiplicacao(fator1: number, fator2: number) {
+  let resultado: number = fator1 * fator2
+  return resultado
+}
+
+function saudacao(nome: string) {
+  let retorno: string = `Olá ${nome}`
+  return retorno
+}
+
+console.log(multiplicacao(25, 68))
+console.log(saudacao("gean"))
